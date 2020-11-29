2a3f11eddccbd736c6f09cdfc07407dd423b8717
Duncan Brown <duncanb@genuinehealth.com>
2020-11-29 15:35:01 -0800

    commit changes

--- src/App.js
+++ src/App.js
@@ -70,13 +70,14 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             //key has to be on the outer method
-            return <ErrorBoundary key={person.id}>
+            return 
               <Person 
                   click={() => this.deletePersonHandler(index)}
                   name={person.name} 
                   age={person.age} 
                   changed={(event) => this.nameChangedHandler(event, person.id)}
-                  /></ErrorBoundary>
+                  key={person.id}
+                  />
           })}
         
         </div>
