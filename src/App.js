3fcbe215cf37f09cb4c9572a1bd0e95d9a91113a
Duncan Brown <duncanb@genuinehealth.com>
2020-11-28 16:55:14 -0800

    using styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'; //lower case words are associated with HTML uppercase w JSX
-import Radium from 'radium';
+//import Radium from 'radium';
+//import {StyleRoot} from 'radium';
 
+import styled from 'styled-components'; // package that mixes css & js very nicely we can use as a react compnonet w styles attached
+
+const StyledButton = styled.button`
+  background-color: ${props=>props.alt ? 'red' : 'green'};
+  font: inherit;
+  border-color: blue;
+  padding: 8px;
+  cursor: pointer;
+  
+  
+  &:hover { // or any psuedo selector but have to start w a ':' this is usable from radium
+    background-color: ${props=>props.alt ? 'salmon' : 'lightgreen'};
+    color: black
+  } 
+`;
 
 class App extends Component {
   //managed within just class, use function properties
@@ -60,11 +76,7 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'green',
-      font: 'inherit',
-      borderColor: 'blue',
-      padding: '8px',
-      cursor: 'pointer'
+
     };
 
     const pageStyle = {
@@ -93,7 +105,13 @@ class App extends Component {
         
         </div>
       );
-      style.backgroundColor = 'red';
+  
+  
+  //    style.backgroundColor = 'red';
+  //    style[':hover'] = {
+  //      backgroundColor: 'salmon',
+  //      color: 'black'
+  //    }
     }
     if (this.state.pageColor){
       style.backgroundColor = 'purple';
@@ -111,19 +129,20 @@ class App extends Component {
 
 
     return (
-      <div 
-        className="App"
-        style={pageStyle}
-        >
-        <h1>Duncan Brown</h1>
-        <p className={classes.join(' ')}>i am here to learn</p>    
-        <button 
-          style = {style}
-          onClick={this.togglePersonsHandler}
-          onDoubleClick={this.toggleBackgroundHandler} >Toggle Name</button>
-        {persons}
-      </div>
+        <div 
+          className="App"
+          style={pageStyle}
+          >
+          <h1>Duncan Brown</h1>
+          <p className={classes.join(' ')}>i am here to learn</p>    
+          <StyledButton 
+            alt={this.state.showPersons}
+            onClick={this.togglePersonsHandler}
+            onDoubleClick={this.toggleBackgroundHandler} >Toggle Name
+            </StyledButton>
+          {persons}
+        </div>
     );}
 }
 
-export default Radium(App);
+export default (App);
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import Person from './Person.css'
+import styled from 'styled-components';
+//import Person from './Person.css';
+
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 24px auto;
+    border: 1 solid #eee;
+    box-shadow: 0 8px 3px #ccc;
+    padding: 16px;
+    text-align: center;
+    
+
+
+    media query is a way to have logic in css if conditions are met : if window width 500px then person width is 450px
+    @media (min-width: 500px) {
+        .Person{
+            width: 450px;
+        }
+`; 
 
 const person = (props) => {
+    /*const style = {
+        '@media (min-width: t00px)':{
+            width: '450px'
+        }
+    };*/
     return (
-        <div className ="Person">
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    )
+       // <div className ="Person" style={style}>
+    <StyledDiv>
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}/>
+    </StyledDiv>
+    );
 };
 
 export default person;
